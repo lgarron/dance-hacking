83c3ef6dca1dc8a12e944f7280d82bd990076e37
Lucas Garron <lucasg@gmx.de>
2012-05-07 16:46:38 -0700

    More verbose output for analyze_full.

--- analyze/analyze_full.sh
+++ analyze/analyze_full.sh
@@ -5,14 +5,20 @@
 FILE_NAME="${1}"
 FILE_TYPE="${FILE_NAME##*.}"
 
-curl \
+echo "Uploading..."
+
+UPLOAD=`curl \
 	--progress-bar \
 	-F "api_key=${ECHO_NEST_API_KEY}" \
 	-F "filetype=${FILE_TYPE}" \
 	-F "track=@${FILE_NAME}" \
-	"http://developer.echonest.com/api/v4/track/upload" > /dev/null
+	"http://developer.echonest.com/api/v4/track/upload"`
+
+echo "Upload response: ${UPLOAD}"
 
 FILE_MD5=`openssl md5 "${FILE_NAME}" | sed "s/^.*= //"`
+
+echo "Checking for the analysis..."
 
 ANALYZE=`curl \
 	-F "api_key=${ECHO_NEST_API_KEY}" \
@@ -20,7 +26,17 @@ ANALYZE=`curl \
 	-F "md5=${FILE_MD5}" \
 	-F "bucket=audio_summary" \
 	"http://developer.echonest.com/api/v4/track/analyze"`
-	
+
+echo
+echo
+echo "${ANALYZE}"
+echo
+echo
+
+
 URL=`echo $ANALYZE | sed "s/^.*analysis_url.: \"\([^\"]*\)\".*\$/\1/"`
+
+echo "Getting the analysis..."
+echo "URL: ${URL}"
 
 curl "${URL}" > "${FILE_NAME}.json"
