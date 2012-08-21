5bd9164afa1964e8dd4504246e0cbfa4ce30ba7d
Lucas Garron <lucasg@gmx.de>
2012-08-21 13:17:17 -0700

    analyze_full checks for existing analysis using has now.

--- analyze/analyze_full.sh
+++ analyze/analyze_full.sh
@@ -6,20 +6,9 @@ FILE_NAME="${1}"
 FILE_TYPE="${FILE_NAME##*.}"
 JSON_CACHE_FOLDER="${HOME}/Documents/Throw/Music/Archive/JSON/"
 
-echo "Uploading..."
-
-UPLOAD=`curl \
-	--progress-bar \
-	-F "api_key=${ECHO_NEST_API_KEY}" \
-	-F "filetype=${FILE_TYPE}" \
-	-F "track=@${FILE_NAME}" \
-	"http://developer.echonest.com/api/v4/track/upload"`
-
-echo "Upload response: ${UPLOAD}"
+echo "Computing hash..."
 
 FILE_MD5=`openssl md5 "${FILE_NAME}" | sed "s/^.*= //"`
-
-echo "Checking for the analysis..."
 
 ANALYZE=`curl \
 	-F "api_key=${ECHO_NEST_API_KEY}" \
@@ -27,6 +16,36 @@ ANALYZE=`curl \
 	-F "md5=${FILE_MD5}" \
 	-F "bucket=audio_summary" \
 	"http://developer.echonest.com/api/v4/track/analyze"`
+
+ALREADY_EXISTED_SUCCESS=`echo "${ANALYZE}" | grep "Success"`
+if [ "${ALREADY_EXISTED_SUCCESS}" = "" ]
+then
+	
+	echo "Uploading..."
+
+	UPLOAD=`curl \
+		--progress-bar \
+		-F "api_key=${ECHO_NEST_API_KEY}" \
+		-F "filetype=${FILE_TYPE}" \
+		-F "track=@${FILE_NAME}" \
+		"http://developer.echonest.com/api/v4/track/upload"`
+
+	echo "Upload response: ${UPLOAD}"
+
+	echo "Checking for the analysis..."
+
+	ANALYZE=`curl \
+		-F "api_key=${ECHO_NEST_API_KEY}" \
+		-F "format=json" \
+		-F "md5=${FILE_MD5}" \
+		-F "bucket=audio_summary" \
+		"http://developer.echonest.com/api/v4/track/analyze"`
+
+else
+
+	echo "Already existed. Getting analysis..."
+
+fi
 
 echo
 echo
