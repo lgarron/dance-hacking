47edcacd01fa19241f99cba4427feec3dda84c38
Lucas Garron <lucasg@gmx.de>
2012-08-27 15:43:27 -0700

    Automatic symlink creation for easy setup.

---
+++ create_links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir links
+
+cd links
+
+ln -s "../tools/bpm.js" "bpm"
+ln -s "../tools/mp3ify" "mp3ify"
+ln -s "../tools/quadify-mp3" "quadify-mp3"
+ln -s "../tools/quadify-wav" "quadify-wav"
+ln -s "../tools/wavify" "wavify"
+ln -s "../tools/wz" "wz"
+ln -s "../tools/wz-beatcaster" "wz-beatcaster"
+ln -s "../tools/wz4" "wz4"
+ln -s "../tools/yt" "yt"
+ln -s "../tools/yt-wz" "yt-wz"
+
+ln -s "../blender/waltz_blender.py" "waltz_blender"
+
+ln -s "../beatcaster/beatcaster.py" "beatcaster"
+
+ln -s "../analyze/analyze_full.sh" "analyze_full.sh"
+ln -s "../analyze/analyze.py" "analyze.py"
+ln -s "../analyze/analyze_full.sh" "analyze"
+
+echo ""
+echo "Add the following to your ~/.bash_profile:"
+echo ""
+PWD=`pwd`
+echo "export PATH=\"${PWD}/links:\$PATH\""
+echo ""
