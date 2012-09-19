c49b875b44016fefb041e39883f268d90a8064a9
Lucas Garron <lucasg@gmx.de>
2012-09-18 22:06:20 -0700

    analyze_full rewritten to make the correct API calls.

--- analyze/analyze_full.sh
+++ analyze/analyze_full.sh
@@ -5,59 +5,61 @@
 FILE_NAME="${1}"
 FILE_TYPE="${FILE_NAME##*.}"
 JSON_CACHE_FOLDER="${HOME}/Documents/Throw/Music/Archive/JSON/"
+CHECKING_SLEEP="2"
 
 echo "Computing hash..."
 
 FILE_MD5=`openssl md5 "${FILE_NAME}" | sed "s/^.*= //"`
 
-ANALYZE=`curl \
-	-F "api_key=${ECHO_NEST_API_KEY}" \
-	-F "format=json" \
-	-F "md5=${FILE_MD5}" \
-	-F "bucket=audio_summary" \
-	"http://developer.echonest.com/api/v4/track/analyze"`
+function get_profile {
+	curl "http://developer.echonest.com/api/v4/track/profile?api_key=${ECHO_NEST_API_KEY}&format=json&md5=${FILE_MD5}&bucket=audio_summary" 2> /dev/null
+}
 
-ALREADY_EXISTED_SUCCESS=`echo "${ANALYZE}" | grep "Success"`
-if [ "${ALREADY_EXISTED_SUCCESS}" = "" ]
-then
-	
-	echo "Uploading..."
-
-	UPLOAD=`curl \
+function post_upload {
+	curl \
 		--progress-bar \
 		-F "api_key=${ECHO_NEST_API_KEY}" \
 		-F "filetype=${FILE_TYPE}" \
 		-F "track=@${FILE_NAME}" \
-		"http://developer.echonest.com/api/v4/track/upload"`
+		"http://developer.echonest.com/api/v4/track/upload"
+}
+
+PROFILE=`get_profile`
+
+ALREADY_EXISTED_SUCCESS=`echo "${PROFILE}" | grep "Success"`
+if [ "${ALREADY_EXISTED_SUCCESS}" = "" ]
+then
+	echo "Uploading..."
+	UPLOAD=`post_upload`
 
 	echo "Upload response: ${UPLOAD}"
-
-	echo "Checking for the analysis..."
-
-	ANALYZE=`curl \
-		-F "api_key=${ECHO_NEST_API_KEY}" \
-		-F "format=json" \
-		-F "md5=${FILE_MD5}" \
-		-F "bucket=audio_summary" \
-		"http://developer.echonest.com/api/v4/track/analyze"`
-
+	PROFILE=`get_profile`
 else
-
-	echo "Already existed. Getting analysis..."
-
+	echo "Already existed. No need to upload."
 fi
 
-echo
-echo
-echo "${ANALYZE}"
-echo
-echo
+while [[ "${PROFILE}" =~ .*"\"status\":".?"\"pending\"".* ]] # Totally fragile hack, but it was better than spending more hours figuring out how to do multipart uploads in Python without an external library.
+do
+	echo "Analysis pending. Checking for it every ${CHECKING_SLEEP} seconds..."
+	sleep "${CHECKING_SLEEP}"
+	PROFILE=`get_profile`
+done
 
+URL=`echo $PROFILE | sed "s/^.*analysis_url.: \"\([^\"]*\)\".*\$/\1/"`
 
-URL=`echo $ANALYZE | sed "s/^.*analysis_url.: \"\([^\"]*\)\".*\$/\1/"`
+# Sanity check: We have the file now?
+if [[ "${PROFILE}" =~ .*"\"status\":".?"\"complete\"".* ]]
+then
+	echo "Downloading the analysis..."
+	echo "URL: ${URL}"
 
-echo "Getting the analysis..."
-echo "URL: ${URL}"
+	curl "${URL}" > "${FILE_NAME}.json" 2> /dev/null
+	cp "${FILE_NAME}.json" "${JSON_CACHE_FOLDER}/${FILE_NAME}.json"
 
-curl "${URL}" > "${FILE_NAME}.json"
-cp "${FILE_NAME}.json" "${JSON_CACHE_FOLDER}/${FILE_NAME}.json"
+	echo "Written to: ${JSON_CACHE_FOLDER}/${FILE_NAME}.json"
+	exit 0
+else
+	echo "Could not get analysis. Sorry. Here's the last response from Echonest:"
+	echo "${PROFILE}"
+	exit 1
+fi
