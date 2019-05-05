08a9af06cbf364952858b140ced3489e370ab498
Lucas Garron <code@garron.net>
2019-05-05 04:39:38 -0700

    React‽

---
+++ pwa-react/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Section!</h1>
+      </section>
+    )
+  }
+}
