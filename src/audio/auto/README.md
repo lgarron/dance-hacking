98ecc4073f72790d4ac76989203e8bae100f5bec
Lucas Garron <code@garron.net>
2023-04-05 04:06:39 -0700

    Remove vestigial audio input code.

--- src/audio/auto/README.md
+++
@@ -1,7 +0,0 @@
-# echonest-analysis.js
-
-Client-side Echonest analysis made easy.
-
-    echonestAnalysis(file, callback)
-
-Makes the necessary API calls to get the `audio_analysis` data, and calls `callback(audio_analysis)`.
--- src/audio/echonest-amazon-proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$call = filter_var($_GET["call"], FILTER_SANITIZE_URL);
-
-echo file_get_contents("https://echonest-analysis.s3.amazonaws.com/".$call);
-echo file_get_contents("https://echonest-analysis.s3.amazonaws.com/TR/TRGOVKX128F7FA5920/3/full.json?Signature=FCyMZ%2B8i26AmoCeSZKCyAkkcBz0%3D&Expires=1335764965&AWSAccessKeyId=AKIAJRDFEY23UEVW42BQ")
-
-?>
